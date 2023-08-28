e6f9f91d6c3076dd564df3a4e8f08835854c1f36
swamy <palaganiayyappa@gmail.com>
2023-08-28 06:57:42 +0530

    'firstcommit'

---
+++ src/main/java/com/example/demo/SbcicdApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SbcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcicdApplication.class, args);
+	}
+
+	@GetMapping("/cicd")
+	public String sayHello()
+	{
+		return "Welcome AWS CI/CD....";
+	}
+}
